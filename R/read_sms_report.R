561237c8113938ee61b8a97ac3db37d4b4b1426f
Jonathan Marshall <j.c.marshall@massey.ac.nz>
2023-06-27 14:54:30 +1200

    factor out the parsing of SMS raw data

--- R/read_sms_report.R
+++ R/read_sms_report.R
@@ -1,3 +1,41 @@
+#' Prase raw data from SMS reporting
+#'
+#' All grades are converted to factors.
+#'
+#' @seealso `read_sms_reporting_csv`
+#'
+#' @param raw_sms a data.frame of data from SMS
+#' @return A tibble with columns `paper`, `mode`, `year`, `semester`,
+#' `number`, `type`, `weight`, `student_id`
+#' `surname`, `firstname`, `mark`, `grade`, `agreed_mark`, `agreed_grade`,
+#' `overall_mark`, `overall_grade`, `agreed_overall_mark`, `agreed_overall_grade`,
+#' `minutes`.
+parse_raw_sms_data <- function(raw_sms) {
+  raw_sms |> dplyr::select(paper = Textbox107,
+                           assessment = Assesment_Type_Label2,
+                           student_id = Student_Number,
+                           surname = Student_Surname,
+                           firstname = Student_Forename,
+                           mark = Student_Assesment_Score1,
+                           grade = Textbox15,
+                           agreed_mark = Student_Assesment_Score3,
+                           agreed_grade = Textbox105,
+                           overall_mark = Student_Paper_Actual_Mark,
+                           overall_grade = Student_Paper_Actual_Grade,
+                           agreed_overall_mark = Student_Paper_Mark,
+                           agreed_overall_grade = Student_Paper_Grade,
+                           minutes = Notes) |>
+    tidyr::extract(assessment,
+                   into=c("number", "type", "weight"),
+                   regex = "\\(([0-9]+)\\) (.*?) \\(([0-9]+).*",
+                   convert = TRUE) |>
+    tidyr::extract(paper,
+                   into=c("paper", "mode", "year", "semester"),
+                   regex="(.*) ?(AKLB|AKLI|DISD|MTUI) ([0-9]+) (.*)") |>
+    dplyr::mutate(paper = stringr::str_trim(paper)) |>
+    dplyr::mutate(dplyr::across(tidyselect::contains('grade'), grade_factor))
+}
+
 #' Read .csv file from SMS reporting
 #'
 #' All grades are converted to factors.
@@ -13,30 +51,7 @@
 #' @export
 read_sms_reporting_csv <- function(path) {
   raw_sms <- readr::read_csv(path)
-
-  raw_sms |> dplyr::select(paper = Textbox107,
-                     assessment = Assesment_Type_Label2,
-                     student_id = Student_Number,
-                     surname = Student_Surname,
-                     firstname = Student_Forename,
-                     mark = Student_Assesment_Score1,
-                     grade = Textbox15,
-                     agreed_mark = Student_Assesment_Score3,
-                     agreed_grade = Textbox105,
-                     overall_mark = Student_Paper_Actual_Mark,
-                     overall_grade = Student_Paper_Actual_Grade,
-                     agreed_overall_mark = Student_Paper_Mark,
-                     agreed_overall_grade = Student_Paper_Grade,
-                     minutes = Notes) |>
-    tidyr::extract(assessment,
-            into=c("number", "type", "weight"),
-            regex = "\\(([0-9]+)\\) (.*?) \\(([0-9]+).*",
-            convert = TRUE) |>
-    tidyr::extract(paper,
-            into=c("paper", "mode", "year", "semester"),
-            regex="(.*) ?(AKLB|AKLI|DISD|MTUI) ([0-9]+) (.*)") |>
-    dplyr::mutate(paper = stringr::str_trim(paper)) |>
-    dplyr::mutate(dplyr::across(tidyselect::contains('grade'), grade_factor))
+  parse_raw_sms_data(raw_sms)
 }
 
 #' Extract paper information from SMS data read using `read_sms_reporting_csv()`
