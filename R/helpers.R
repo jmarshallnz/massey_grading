c5b23dc3444b64e2d89c8d3b8d73437f36c2f9cd
Jonathan Marshall <j.c.marshall@massey.ac.nz>
2023-06-28 15:15:37 +1200

    adds NF as a grade possibility

--- R/helpers.R
+++ R/helpers.R
@@ -3,21 +3,22 @@
 #' @return a factor variable ordered appropriately
 #' @export
 grade_factor <- function(grades) {
-  factor(grades, levels=c("A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "E", "MA", "F", "DC", "AG", "WD"))
+  factor(grades, levels=c("A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "E", "MA", "F", "DC", "AG", "WD", "NF"))
 }
 
-#' Computes the grade category (A, B, C, F, DC) for a vector of grades
+#' Computes the grade category (A, B, C, F, DC, NF) for a vector of grades
 #' @param grades the grades as a character vector
 #' @return a factor variable with grade categories
 #' @export
 grade_category <- function(grades) {
   grade_cat <- dplyr::case_when(
+                        grades == "NF" ~ "NF",
                         grades %in% c("MA", "DC", "WD") ~ "DC",
                         grades %in% c("D", "E") ~ "F",
                         grades == "AG" ~ "C",
                         TRUE ~ stringr::str_sub(grades, 1, 1)
                         )
-  factor(grade_cat, levels = c("A", "B", "C", "F", "DC"))
+  factor(grade_cat, levels = c("A", "B", "C", "F", "DC", "NF"))
 }
 
 #' Round a mark to the nearest integer
